d8dd59ae03800a69d5a3dbc559992cbd21b83132
Zaman-A-Piri Pasa <zaman-a-piripasa@Zaman-A-Piris-MacBook-Pro>
2020-12-16 11:50:15 +0600

    Implement Checkout service. It will be used for scan item, apply price rules, get total

--- src/Services/Checkout.php
+++ src/Services/Checkout.php
@@ -6,17 +6,47 @@ namespace App\Services;
 
 use App\Entities\Item;
 use App\Interfaces\CheckoutInterface;
+use App\Interfaces\RepositoryInterface;
+use App\Rules\AbstractPriceRules;
 
 class Checkout implements CheckoutInterface
 {
+    private $rulesRepository;
+    private $cart;
 
-    public function scan(Item $item)
+    /**
+     * Checkout constructor.
+     * Use dependency injection, so that different rules can be used
+     * @param RepositoryInterface $repository
+     */
+    public function __construct(RepositoryInterface $repository)
     {
-        // TODO: Implement scan() method.
+        $this->rulesRepository = $repository;
+        $this->cart = new Cart();
     }
 
+    /**
+     * Scan item to add to cart
+     * @param Item $item
+     */
+    public function scan(Item $item)
+    {
+        $this->cart->addItem($item);
+    }
+
+    /**
+     * Get total price after applying rules
+     * @return int
+     */
     public function total()
     {
-        // TODO: Implement total() method.
+        $total = 0;
+        foreach ($this->rulesRepository->getAll() as $priceRule) {
+            if ($priceRule instanceof AbstractPriceRules) {
+                $total = $priceRule->calculateTotal($this->cart);
+            }
+        }
+
+        return $total;
     }
 }
