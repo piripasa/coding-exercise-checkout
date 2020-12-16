00591fc23c0b021b7120553d769c46bafb205667
Zaman-A-Piri Pasa <zaman-a-piripasa@Zaman-A-Piris-MacBook-Pro>
2020-12-16 10:51:39 +0600

    Implement item bundle price rule. buy n of them, and they’ll cost you less than buying them individually

--- src/Rules/BundlePriceRule.php
+++ src/Rules/BundlePriceRule.php
@@ -8,9 +8,40 @@ use App\Interfaces\CartInterface;
 
 class BundlePriceRule extends AbstractPriceRules
 {
+    private $rules;
 
+    /**
+     * BundlePriceRule constructor.
+     * @param array $rules
+     */
+    public function __construct(array $rules)
+    {
+        $this->rules = $rules;
+    }
+
+    /**
+     * Calculate total based on rules
+     * @param CartInterface $cart
+     * @return float|int
+     */
     public function calculateTotal(CartInterface $cart)
     {
-        // TODO: Implement calculateTotal() method.
+        $countCart = [];
+        $totalPrice = 0;
+
+        foreach ($cart->getItems() as $item) {
+            if (!isset($countCart[$item->getSku()])) {
+                $countCart[$item->getSku()] = 0;
+            }
+
+            $countCart[$item->getSku()]++;
+            $totalPrice += $item->getUnitPrice();
+            if (isset($this->rules[$item->getSku()]) && $countCart[$item->getSku()] == $this->rules[$item->getSku()]['count']) {
+                $totalPrice -= $this->rules[$item->getSku()]['count'] * $item->getUnitPrice();
+                $totalPrice += $this->rules[$item->getSku()]['price'];
+                $countCart[$item->getSku()] = 0;
+            }
+        }
+        return $totalPrice;
     }
 }
